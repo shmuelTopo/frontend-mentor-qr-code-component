aac74ae244855d87f550000c57eca15e82418703
Shmuel Toporowitch <shmuel.dev@gmail.com>
2026-06-28 10:44:56 -0400

    Add GitHub Actions workflow for deploying Astro site and update Layout styling and Html to be more semantic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  base: '/frontend-mentor-qr-code-component/',
 });
